e10db9ea3df9c2e6281e7b382073ce0a65d631fc
gyeyeon <iam.gyeyeon@gmail.com>
2024-08-04 20:11:06 +0900

    백준 step 6 진행중

---
+++ src/algorithm/com/challenges/baekjoon/_06_deepening/No25083.java
@@ -0,0 +1,15 @@
+package algorithm.com.challenges.baekjoon._06_deepening;
+
+public class No25083 {
+    /**
+     * https://www.acmicpc.net/problem/25083
+     */
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
